2868c6e0ea6b0e57bec0c7727b639a301d0c69b3
Gissel <gisselc526@gmail.com>
2026-04-06 13:42:18 -0600

    ultimas modificaciones

--- dom.js
+++ dom.js
@@ -541,6 +541,73 @@ function videoInteligente() {
     
 }
 
+// Ejercicio 20 - Validacion de formulario
+function validarForm() {
+    const form = document.querySelector(".form");
+    const inputs = document.querySelectorAll(".form [required]");
+
+    inputs.forEach(input => {
+        const span = document.createElement("span");
+        span.id = input.name;
+        span.textContent = input.title;
+        span.classList.add("form-error", "none");
+        input.insertAdjacentElement("afterend", span);
+    });
+
+    document.addEventListener("focusout", (e) => {
+        if(e.target.matches(".form [required]")) {
+
+            let input = e.target;
+            let pattern = input.pattern || input.dataset.pattern;
+            let errorSpan = document.getElementById(input.name);
+
+            if(input.value === "") {
+                errorSpan.classList.remove("activo");
+                return;
+            }
+
+            if(pattern) {
+                const expR = new RegExp(pattern);
+
+                if(!expR.test(input.value)) {
+                    errorSpan.textContent = input.dataset.error || input.title;
+                    errorSpan.classList.add("activo");
+                } else {
+                    errorSpan.classList.remove("activo");
+                }
+            }
+
+            if(!pattern) {
+                if(input.value === "") {
+                    errorSpan.textContent = input.title;
+                    errorSpan.classList.add("activo");
+                } else {
+                    errorSpan.classList.remove("activo");
+                }
+            }
+        }
+    });
+
+    document.addEventListener("submit", (e) => {
+        //e.preventDefault();
+        //alert("Enviando Datos");
+
+        const loader = document.querySelector(".form-loader");
+        const res = document.querySelector(".form-respuesta");
+
+        loader.classList.remove("none");
+        
+        setTimeout(() => {
+            loader.classList.add("none");
+            res.classList.remove("none");
+            form.reset();
+
+            setTimeout(() => res.classList.add("none") , 2000);
+
+        }, 3000);
+    });
+}
+
 function initEvents() {
     scrollMenu();
 
@@ -589,6 +656,8 @@ function initEvents() {
     scrollSpy();
 
     videoInteligente();
+
+    validarForm();
 }
 
 
